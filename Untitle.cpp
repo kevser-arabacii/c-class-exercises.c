ba9774b2ae5bf4d4465f13486aaf3a7b71cea552
Kevser Arabacı <kxvsrr@gmail.com>
2026-03-04 01:42:30 +0300

    Add files via upload

---
+++ Untitle.cpp
@@ -0,0 +1,16 @@
+//KULLANICI TARAFINDAN GÝRÝLEN SAYIYI EKRANA YAZDIRMA 
+ #include <stdio.h>
+  
+  int main(){
+	
+	int sayi;
+	
+	/*kullanýcýdan sayý al*/
+	printf("lutfen sayi degeri giriniz :");
+	scanf("%d",&sayi );
+	 
+	 /*sayý deðerini yazdýr*/
+	 printf("Girdiginiz sayi degeri : %d",sayi);
+	  
+	  return 0;
+    }
